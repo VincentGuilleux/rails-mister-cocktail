5fb74d1daedd90a9bd91e1c38f507abf560b5384
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-02 12:18:24 +0200

    seeding manual ingredients ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Applejack")
+Ingredient.create(name: "Sweet Vermouth")
+Ingredient.create(name: "Strawberry schnapps")
+Ingredient.create(name: "Apricot brandy")
+Ingredient.create(name: "Southern Comfort")
+Ingredient.create(name: "Jack Daniels")
+
