9cef4d2bdac4096dc3f87003cc938afca31e67d3
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-03 12:47:09 +0200

    Galère

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 Ingredient.create(name: "Light rum")
 Ingredient.create(name: "Applejack")
@@ -17,4 +16,21 @@ Ingredient.create(name: "Strawberry schnapps")
 Ingredient.create(name: "Apricot brandy")
 Ingredient.create(name: "Southern Comfort")
 Ingredient.create(name: "Jack Daniels")
+gin = Ingredient.create(name: "Gin")
+tw = Ingredient.create(name: "tonic water")
 
+gin_tonic = Cocktail.create(name: "Gin Tonic")
+mojito = Cocktail.create(name: "Mojito")
+
+dose1gt = Dose.new(description: '4 cl', ingredient_id: gin.id)
+dose2gt = Dose.new(description: '10 cl', ingredient_id: tw.id)
+dose3gt = Dose.new(description: '1', ingredient_id: lemon.id)
+dose4gt = Dose.new(description: '2', ingredient_id: ice.id)
+dose1gt.cocktail = gin_tonic
+dose2gt.cocktail = gin_tonic
+dose3gt.cocktail = gin_tonic
+dose4gt.cocktail = gin_tonic
+dose1gt.save!
+dose2gt.save!
+dose3gt.save!
+dose4gt.save!
